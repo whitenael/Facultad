85611e9a9074cb46649afe7769f77b9de7053edd
luismanuelvera <antoine.windows2@gmail.com>
2024-05-21 20:33:45 -0300

    se suben ejercicio1 de go

---
+++ Go/Practica 2/obligatorios/ejercicio1/Stack.go
@@ -0,0 +1,73 @@
+package main
+
+// definimos la interfaz
+type Collection[T any] interface {
+	IsEmpty() bool
+	Len() int
+	ToString() string
+	Iterate(func(int) int) int
+}
+
+// definimos el tipo
+type Stack[T any] struct {
+	data LinkedList[T]
+}
+
+// definimos los metodos
+func New[T any]() Stack[T] {
+	return Stack[T]{}
+}
+
+func (self Stack[T]) IsEmpty() bool {
+	return self.data.IsEmpty()
+}
+
+func (self Stack[T]) Len() int {
+	return self.data.Len()
+}
+
+func (self Stack[T]) ToString() string {
+	return self.data.ToString()
+}
+
+// definimos la pila como una FIFO
+func (self *Stack[T]) Push(dato T) {
+	self.data.PushFront(dato)
+}
+
+// definimos el pop de estilo generico
+func (self *Stack[T]) Pop() (T, bool) {
+	if self.IsEmpty() {
+		var zero T
+		return zero, false
+	}
+
+	data := self.data.FrontElement()
+	self.data.Remove()
+	return data, true
+}
+
+// definimos el poll de estilo generico (es igual al pop, solamente que este no devuelve un booleano)
+func (self *Stack[T]) Poll() T {
+	if self.IsEmpty() {
+		var zero T
+		return zero
+	}
+
+	data := self.data.FrontElement()
+	self.data.Remove()
+	return data
+}
+
+func (self *Stack[T]) FrontElement() (T, bool) {
+	if self.IsEmpty() {
+		var zero T
+		return zero, false
+	}
+
+	return self.data.FrontElement(), true
+}
+
+func (self *Stack[T]) Iterate(f func(T)) {
+	self.data.Iterate(f)
+}
